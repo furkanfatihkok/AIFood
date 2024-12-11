ddf540df54114e6597e2ba99b85de70819683fbf
furkanfatihkok <furkankok296@gmail.com>
2024-12-11 10:49:08 +0300

    Remove user-specific files and update .gitignore

--- AIFood/Modules/Onboarding/OnboardingViewController.swift
+++ AIFood/Modules/Onboarding/OnboardingViewController.swift
@@ -11,6 +11,6 @@ final class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
     }
 }
